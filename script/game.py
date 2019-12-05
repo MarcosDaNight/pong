695b983b3fe226d9ae16b801b01bd8fade3e952b
RaissonSouto <raissonsouto@outlook.com>
2019-12-05 02:39:15 -0300

    removi coisas inuteis e to tentando melhorar o movimento da bola e as colissoes

--- script/game.py
+++ script/game.py
@@ -1,4 +1,5 @@
-import pygame, functions, random
+import pygame, random
+from functions import text,colors,exit
 from math import sqrt
 from pygame.locals import *
 
@@ -7,12 +8,12 @@ pygame.init()
 def game():
 	class elements():
 
-		color = functions.colors['white']
+		color = colors['white']
 		width = 25
 		height = 100
 		posx = 100
 		posy = 250
-		speedy = 1
+		speedy = 3
 
 		def colide(self,object):
 
@@ -24,7 +25,7 @@ def game():
 		def draw(self):
 			pygame.draw.rect(screen, self.color,(self.posx,self.posy,self.width,self.height))
 
-	class bal(elements):
+	class ball(elements):
 
 		speedx = 0
 
@@ -34,18 +35,25 @@ def game():
 
 		def speed(self):
 
-			self.speedx = random.uniform(-1,1)
+			#falta variar o negativo positivo pro y
 
-			if self.speedx <= -0.8 or self.speedx == 0 or self.speedx >= 0.8:
-				self.speedx = 0.8
+			self.speedx = random.uniform(-0.8,0.8)
 
-			self.speedy = sqrt(1-self.speedx**2)*5
+			if self.speedx <= 0.25 and self.speedx >= -0.25:
+				self.speedx = random.choice([0.8,-0.8])
 
-			self.speedx = self.speedx*5
+			self.speedy = sqrt(1-self.speedx**2)*3
+
+			self.speedx = self.speedx*3
+
+		def move(self):
+			self.speedx += 1
+			self.speedy += 1
+			self.posx += self.speedx
+			self.posy += self.speedy
 
 	class player(elements):
 
-		nickname = 'player'
 		score = 0
 
 		def limit(self):
@@ -69,54 +77,36 @@ def game():
 	screenSize = screenWidth, screenHeight = 1000, 600
 	screen = pygame.display.set_mode(screenSize)
 
-	# definindo player1
-
 	player1 = player()
-	player1.nickname = 'Raisson'
-
-	# definindo player2
 
 	player2 = player()
-	player2.nickname = 'Marcos'
 	player2.posx = 875
 
-	# definindo ball
-
-	ball = bal()
+	ball = ball()
 	ball.height = 25
 	ball.posx = 480
 	ball.posy = 288
 	ball.speed()
 
-	# loop q mantem o jogo
-
 	while True:
-		functions.exit()
+		clock = pygame.time.Clock()
+		clock.tick(120)
+		exit()
 
-		# captura os clicks no teclado
 		key = pygame.key.get_pressed()
 
-		# Movimentacao Player 1
 		player1.move(K_w,K_s,key)
-
 		player1.limit()
 
-		# Movimentacao Player 2
 		player2.move(K_UP,K_DOWN,key)
-
 		player2.limit()
 
-		ball.posx += ball.speedx
-		ball.posy += ball.speedy
-
-		# colissao
+		ball.limit()
+		ball.move()
 
 		player1.colide(ball)
 		player2.colide(ball)
 
-		ball.limit()
-
-		### tirar o choice e fazer um pitagoras no lugar
 		### mudar o speed da bola
 		### ajeitar as funcoes de colissao
 
@@ -124,27 +114,24 @@ def game():
 			ball.speed()
 			ball.posx = 488
 			ball.posy = 288
-			#pygame.time.wait(10000) ###ajeitar isso pq ta travado ate pra fechar o jogo
+			#pygame.time.delay(1000) ###ajeitar isso pq ta travado ate pra fechar o jogo
 			player2.score += 1
 
 		elif ball.posx + ball.width >= screenWidth:
 			ball.speed()
 			ball.posx = 488
 			ball.posy = 288
-			#pygame.time.wait(10000)
+			#pygame.time.delay(1000)
 			player1.score += 1
 
-		#condicao de parada
 		if player1.score == 5 or player2.score == 5:
 			break
 
-		# parte q faz as coisas aparecerem na tela
-
-		screen.fill(functions.colors['black'])
+		screen.fill(colors['black'])
 		player1.draw()
 		player2.draw()
 		ball.draw()
-		functions.text('%d x %d' %(player1.score,player2.score), screen, 45, 480, 60)
+		text('%d x %d' %(player1.score,player2.score), screen, 45, 480, 60)
 
 		pygame.display.update()
 
